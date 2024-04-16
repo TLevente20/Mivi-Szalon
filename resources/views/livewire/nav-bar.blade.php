8e58a36000daf6e608033a95d8f39ce53a6deb64
TLevente20 <takacs.levente1015@gmail.com>
2024-04-16 20:31:16 +0200

    Nav bar waiting

--- resources/views/livewire/nav-bar.blade.php
+++ resources/views/livewire/nav-bar.blade.php
@@ -1,10 +1,13 @@
 <div>
+    @php
+        $currentPage = $path = request()->path();
+    @endphp
     <div class="flex justify-between  pr-24 pl-8">
         <a href="{{ route('home') }}"><img src="{{ asset('img/mivi.logo.png') }}" alt="Mivi logo" class=" h-[76px]"></a>
         <div class="flex text-[15px] my-auto font-medium">
-            <a href="{{ route('home') }}" class="ml-14">Főoldal</a>
+            <a href="{{ route('home') }}" class="{{ $currentPage === '/' ? 'font-semibold': ''}} ml-14">Főoldal</a>
             <a href="#" class="ml-14">Rólunk</a>
-            <a href="{{ route('hazirend') }}" class="ml-14">Házirend</a>
+            <a href="{{ route('hazirend') }}" class="{{ $currentPage === 'hazirend' ? 'font-semibold': ''}} ml-14">Házirend</a>
             <a href="#" class="ml-14">Kapcsolat</a>
             <a href="#" class="ml-14">Kutyasuli</a>
             <a href="#" class="ml-14">Galéria</a>
